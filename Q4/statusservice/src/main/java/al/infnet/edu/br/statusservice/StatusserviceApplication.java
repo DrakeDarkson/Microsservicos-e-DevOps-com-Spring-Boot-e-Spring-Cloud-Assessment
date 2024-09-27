55900886987afd418285d5867e212566f62b968b
breno <breno.mota@al.infnet.edu.br>
2024-09-27 03:59:15 -0300

    Fixing Q4 commit

---
+++ Q4/statusservice/src/main/java/al/infnet/edu/br/statusservice/StatusserviceApplication.java
@@ -0,0 +1,12 @@
+package al.infnet.edu.br.statusservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatusserviceApplication.class, args);
+	}
+}
---
+++ Q5/statusservice/src/test/java/al/infnet/edu/br/statusservice/StatusserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package al.infnet.edu.br.statusservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatusserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
